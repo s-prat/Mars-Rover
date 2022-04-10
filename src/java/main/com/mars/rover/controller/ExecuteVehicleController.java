5f1d71bcc8b5a9d0f38e62875e2c6b50306839f6
Pratibha <psharma@atlassian.com>
2022-04-11 00:40:21 +0100

    started writing first test

--- src/java/main/com/mars/rover/controller/ExecuteVehicleController.java
+++ src/java/main/com/mars/rover/controller/ExecuteVehicleController.java
@@ -1,4 +1,6 @@
 package com.mars.rover.controller;
 
 public interface ExecuteVehicleController {
+
+    String moveVehicle(String inputCoordinates, String commands);
 }
--- src/java/main/com/mars/rover/controller/ExecuteVehicleControllerImpl.java
+++ src/java/main/com/mars/rover/controller/ExecuteVehicleControllerImpl.java
@@ -1,4 +1,23 @@
 package com.mars.rover.controller;
 
 public class ExecuteVehicleControllerImpl implements ExecuteVehicleController {
+    @Override
+    public String moveVehicle(String inputCoordinates, String commands) {
+        return "";
+    }
+
+    public String turnLeft(String direction) {
+
+        return null;
+    }
+
+    public String turnRight(String direction) {
+
+        return null;
+    }
+
+    public String moveOneStep(String inputCoordinates) {
+
+        return null;
+    }
 }
